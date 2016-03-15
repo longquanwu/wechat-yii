6f30c6a19ce7fc897509e894e6e1ede39d40bfe6
longquanwu <longquangege@foxmail.com>
2016-03-15 15:13:30 +0800

    修改.gitignore

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1:3390;dbname=wechat',
-    'username' => 'root',
-    'password' => 'w314314',
-    'charset' => 'utf8',
-];
